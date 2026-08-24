b88331ef8e43ce6cd946b2d1ade3b143c8871341
Klyne Chrysler <klynechrislu@gmail.com>
2026-08-24 15:10:57 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        left, right = {}, {}
+
+        if len(s) == 0:
+            return False
+
+        ct = "".join(char for char in s if char.isalnum()).lower()
+        
+        reversed_ct = ct[::-1]
+
+        print(reversed_ct)
+
+        if ct == reversed_ct:
+            return True
+        else: 
+            return False
+        
